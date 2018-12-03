20ef0a0ac3fba69c09efe1928b62862449c4246a
tero-yrjola <yrjola.tero@gmail.com>
2018-12-03 20:33:32 +0200

    Add some static text and links to App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1> Welcome! </h1>
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Click the links below:
+                    </p>
+                    <ul className="Link-list">
+                        <li>
+                            <a
+                                className="App-link"
+                                href="https://google.com"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Google
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                className="App-link"
+                                href="https://stackoverflow.com/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Stack Overflow
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                className="App-link"
+                                href="https://www.wikipedia.org/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Wikipedia
+                            </a>
+                        </li>
+                    </ul>
+
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;

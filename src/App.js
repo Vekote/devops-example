2d06d6b6cf9db1db656580c33257befc1e0d353d
tero-yrjola <yrjola.tero@gmail.com>
2018-12-03 21:03:41 +0200

    Add title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                    <h1> Welcome! </h1>
+                    <h1 className="Title"> Welcome! </h1>
                     <img src={logo} className="App-logo" alt="logo"/>
                     <p>
                         Click the links below:

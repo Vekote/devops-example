f76ae99642f40a4ccfdad0f8803871abf3489482
tero-yrjola <yrjola.tero@gmail.com>
2018-12-03 21:03:49 +0200

    Add some simple tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
+configure({ adapter: new Adapter() });
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    shallow(<App />);
+});
+
+it('renders the title', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('.Title').length).toBe(1);
+});
+
+it('renders three links', () => {
+   const wrapper = shallow(<App/>);
+   expect(wrapper.find('.App-link').length).toBe(3);
 });
